3ffda7b9aaefbcf7bb2aaad31d5cbe489573727c
Marty <martymccann@live.co.uk>
2020-11-14 11:15:42 +0000

    Initial commit

---
+++ src/main/java/com/northpole/remotesecretsanta/RemoteSecretSantaApplication.java
@@ -0,0 +1,13 @@
+package com.northpole.remotesecretsanta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemoteSecretSantaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemoteSecretSantaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/northpole/remotesecretsanta/RemoteSecretSantaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.northpole.remotesecretsanta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemoteSecretSantaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
